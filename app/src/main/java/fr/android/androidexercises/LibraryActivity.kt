9e056f61df3e19ffb047c6043b25edf99b96415b
mousha <nfrays16@etudiant.mines-nantes.fr>
2018-11-16 13:42:38 +0100

    fragment du cul

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -1,18 +1,40 @@
 package fr.android.androidexercises
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class LibraryActivity : AppCompatActivity(), Step0Fragment.OnNextStep0Listener {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
+
+        supportFragmentManager.beginTransaction().replace(R.id.containerFrameLayout, Step0Fragment(), Step0Fragment::class.java.name).commit()
         // TODO replace Step0Fragment in containerFrameLayout
+        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            supportFragmentManager.beginTransaction().replace(R.id.containerFrameLayout2, Step1Fragment(), Step1Fragment::class.java.name).commit()
+        }
+
+
 
     }
 
-    // TODO implement onNext() from Step0Fragment.OnNextStep0Listener
+
+    override fun onNext() {
+        // TODO implement onNext() from Step0Fragment.OnNextStep0Listener
+
+        supportFragmentManager.beginTransaction().replace(R.id.containerFrameLayout, Step1Fragment(), Step1Fragment::class.java.name).addToBackStack(Step0Fragment::class.java.name).commit()
+
+    }
+
+
+    override fun onBackPressed() {
+        supportFragmentManager.popBackStack()
+    }
+
+
 
 }
--- app/src/main/java/fr/android/androidexercises/Step0Fragment.kt
+++ app/src/main/java/fr/android/androidexercises/Step0Fragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Exception
 
 class Step0Fragment : Fragment() {
 
@@ -15,23 +16,30 @@ class Step0Fragment : Fragment() {
         private val STEP_0 = "This is step 0"
     }
 
-    private val textView: TextView? = null
-    private val listener: OnNextStep0Listener? = null
+    private var textView: TextView? = null
+    private var listener: OnNextStep0Listener? = null
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         // TODO cast context to listener
+        when(context){
+            is OnNextStep0Listener -> listener = context
+            else -> throw Exception("pas le bon uesh")
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_step0, container, false)
 
         // TODO find TextView and set text
+        textView = view.findViewById(R.id.textView)
 
         // TODO find Button and set listener
         val nextButton: Button
+        nextButton = view.findViewById(R.id.nextButton)
         nextButton.setOnClickListener {
             // TODO call listener
+            listener?.onNext()
         }
 
         return view
@@ -41,8 +49,12 @@ class Step0Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // TODO setText(STEP_0)
+        textView.apply { this?.text = STEP_0 }
     }
 
-    interface OnNextStep0Listener// TODO add onNext() method
+    interface OnNextStep0Listener {
+        // TODO add onNext() method
+        fun onNext()
+    }
 
 }
